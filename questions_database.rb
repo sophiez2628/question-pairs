92126ff4f45571505e260684dc5a5110ffbaf4fd
sophie zhao <sophie.zhao@ucla.edu>
2015-09-08 16:23:26 -0700

    Finish question followed

--- questions_database.rb
+++ questions_database.rb
@@ -117,6 +117,10 @@ class Question
   def followers
     QuestionFollow.followers_for_question_id(@id)
   end
+
+  def self.most_followed(n)
+    QuestionFollow.most_followed_questions(n)
+  end
 end
 
 class QuestionFollow
